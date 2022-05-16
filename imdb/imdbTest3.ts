4ae658c2263378551d13904327e0e33026458a2d
cardenalcopia <kirisse85@gmail.com>
2022-05-16 19:05:28 +0200

    Subida 4 imdb

--- imdb/imdbTest3.ts
+++ imdb/imdbTest3.ts
@@ -3,3 +3,9 @@ import { imdb } from "./imdbTest2";
 let pelicula1 = new Movie("Fight club",1999,"Brad Pitt ","USA","flinch","flinch","english","Netflix",false,"Brad pitt","coppola","Fox","Action");
 let pelicula2 = new Movie("El señor de los anillos",2002,"Ian Mcklein y Vigo Mortensen","USA","Peter Jackson","Tolkien","English","HBO",false,"Frodo","Warner","Warner","Adventure");
 let imdb1 = new imdb([pelicula1,pelicula2]);
+let imdbbd =JSON.stringify(imdb1);
+
+
+let fs=require('fs');
+
+fs.writeFileSync("imdbBBDD.json", imdbbd);
